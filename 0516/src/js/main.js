049daa1948b5b8b05b2e82146ca77c495ffb16d1
zanjs <root@zanjs.com>
2017-05-17 11:41:25 +0800

    There is a great different between exposure of the mind and that of the body :heart_eyes_cat:

--- 0516/src/js/main.js
+++ 0516/src/js/main.js
@@ -21,27 +21,34 @@ function webSiteMore() {
 function ClickSearch() {
 
   $("#search-btn").on("click", function() {
-    var keyInput = $("#search-key")
-    var key = trim(keyInput.val())
-
-    if (key == "") {
-      urlErrMsg("请输入商品网址")
-      return
-    }
-
-    if (!checkURL(key)) {
-      urlErrMsg("请输入正确的网址")
-      return
-    }
-
-    Loader()
-
-    setTimeout(function() {
-      Loader("1")
-    }, 2000);
-
+    searchGO()
   })
+  $('#search-key').bind('keypress', function(event) {
+    if (event.keyCode == "13") {
+      searchGO()
+    }
+  })
+}
 
+function searchGO() {
+  var keyInput = $("#search-key")
+  var key = trim(keyInput.val())
+
+  if (key == "") {
+    urlErrMsg("请输入商品网址")
+    return
+  }
+
+  if (!checkURL(key)) {
+    urlErrMsg("请输入正确的网址")
+    return
+  }
+
+  Loader()
+
+  setTimeout(function() {
+    Loader("1")
+  }, 2000);
 }
 
 ;
