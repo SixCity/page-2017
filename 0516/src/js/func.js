3ea01416d617cb810460c32aabcfa7a4ed27a8c3
zanjs <root@zanjs.com>
2017-05-17 11:36:17 +0800

    回滚版本到分支中的某一个commit时间点 git reset [commit 哈希值] :sun_with_face:

--- 0516/src/js/func.js
+++ 0516/src/js/func.js
@@ -36,3 +36,13 @@ function urlErrMsg(msg) {
   layer.msg(msg);
   $("#search-key").focus()
 }
+
+function isIE9() {
+
+  if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE6.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE7.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0" || navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE9.0") {
+    alert("您的浏览器版本过低，无法保证购物安全，请下载IE9以上版本");
+  }
+
+}
+
+isIE9()
