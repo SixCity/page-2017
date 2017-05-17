b09a5f5f9d80d0256952aed3d1badf9b6f5f0ef4
zanjs <root@zanjs.com>
2017-05-17 11:08:13 +0800

    回滚版本到分支中的某一个commit时间点 git reset [commit 哈希值] :octopus:

--- 0516/src/js/func.js
+++ 0516/src/js/func.js
@@ -24,6 +24,14 @@ function checkURL(URL) {
   }
 }
 
+// url 参数
+function GetQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
+}
+
 function urlErrMsg(msg) {
   layer.msg(msg);
   $("#search-key").focus()
